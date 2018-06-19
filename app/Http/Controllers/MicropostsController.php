7f42cda3c7a710918872795a244bd0bce2391985
kaho.kobayashi <kaho.kobayashi@rakuten.com>
2018-06-19 03:22:51 +0000

    follow, unfollow

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -30,7 +30,7 @@ class MicropostsController extends Controller
             return view('welcome');
         }
     }
-     public function store(Request $request)
+        public function store(Request $request)
     {
         $this->validate($request, [
             'content' => 'required|max:191',
@@ -42,7 +42,7 @@ class MicropostsController extends Controller
 
         return redirect()->back();
     }
-    public function destroy($id)
+        public function destroy($id)
     {
         $micropost = \App\Micropost::find($id);
 
